939e50fb2ebebeef00d712cb17ed10fc236065c1
tomaszhof <tomaszhof@gmail.com>
2014-07-04 14:11:38 +0200

    + modified DICOM client

--- scripts/dicomClient.py
+++ scripts/dicomClient.py
@@ -21,16 +21,14 @@ def getDicomList(sUID):
   #print data
   return data.split('\n')
 
-def getData(p): #(fileList):
+def getData(p):
   from datetime import tzinfo, timedelta, datetime
   (suid, furl) = p
   t1 = datetime.now()
   t2 = t1
-  #noFiles = len(fileList)
   tSum = 0
   fSize = 0
   resultsList = []
-  #for (suid,furl) in fileList:
   if (furl == ''):
     return []
   fname = furl.split('/')[-1]
@@ -40,7 +38,6 @@ def getData(p): #(fileList):
     t2 = datetime.now()
     fSize = h['content-length']
   except IOError:
-    #sleep(5)
     t1 = datetime.now()
     (n,h) = urllib.urlretrieve (furl, "/home/tomhof/dest/"+fname)
     t2 = datetime.now()
@@ -52,7 +49,7 @@ def getData(p): #(fileList):
   return resultsList
 
 def writeResultsList(resultList):
-  f_name = 'dicomClientTimeLogForFiles.txt'
+  f_name = 'dicomClientStats.txt'
   f = open(f_name,'a')
   for (suid, fname, fSize, tdeltaMS) in resultList:
     f.write(str(suid) + ';' + fname + ';' + str(fSize) + ';' + str(tdeltaMS) + '\n')
@@ -65,24 +62,45 @@ def joinResultLists(listOfLists):
     resultList += l
   return resultList
 
+
+def getSeriesInstanceUIDsFromCsv():
+  f_name = 'seriesInstanceUIDs.csv'
+  f = open(f_name,'r')
+  suids = f.readlines()
+  return suids
+  
 def getDicoms():
   rootFolder = "/home/tomhof/tmp/dicom/"
-  seriesInstanceUIDs = getSeriesInstanceUIDs(rootFolder)
+  #seriesInstanceUIDs = getSeriesInstanceUIDs(rootFolder)
+  #seriesInstanceUIDs = seriesInstanceUIDs[1:]
+  seriesInstanceUIDs = getSeriesInstanceUIDsFromCsv()
+  
+  #f_name = 'seriesInstanceUIDs.csv'
+  #f = open(f_name,'w')
+  #for suid in seriesInstanceUIDs[1:]:
+  #  f.write(suid + '\n')
+  #f.close()
+
   allPairList = []
-  for sUID in seriesInstanceUIDs[1:]:
+  for sUID in seriesInstanceUIDs:
     #print '-----' + str(sUID) + '---------\n'
     fileList = getDicomList(sUID)
     pairList = map(lambda x : (sUID, x), fileList)
+    pairList = filter(lambda (x,y) : y!='', pairList)
     allPairList = allPairList + pairList
     #print '---------END----------------\n'
   return allPairList
 
 def main():
-  #hospital = sys.argv[1]
+  from datetime import tzinfo, timedelta, datetime
   print 'Get DICOMs list...'
+  t01 = datetime.now()
   allPairList = getDicoms()
+  t02 = datetime.now()
+  tdelta0 = t02-t01
+  tdeltaMS0 = int(tdelta0.total_seconds()*1000) #in miliseconds
   print 'done.'
-  #prepareStats()
+  #print allPairList
   
   #step (pool size)
   step = 10
@@ -91,23 +109,46 @@ def main():
   #download by splitting
   currBeg = 0
   currEnd = 0
+  nextEnd = currEnd + step
   maxL = len(allPairList)
-  while currEnd < maxL :
+  t1 = datetime.now()
+  while nextEnd < maxL :
     currBeg = currEnd
-    currEnd = currEnd + step 
+    currEnd = currEnd + step
+    nextEnd = currEnd + step
     subList = allPairList[currBeg:currEnd]
     resArr = pool.map(getData,subList)
     resultList = joinResultLists(resArr)
     writeResultsList(resultList)
     print 'Downloaded ' + str(currEnd) + '/' + str(maxL) + '\n'
-   
+  t2 = datetime.now()
+  tdelta = t2-t1
+  tdeltaMS = int(tdelta.total_seconds()*1000) #in miliseconds
+
   rest = maxL - int(maxL/step)*step;
-  subList = allPairList[maxL-rest:maxL]
+  currBeg = maxL-rest
+  currEnd = maxL
+  subList = allPairList[currBeg:currEnd]
   resArr = pool.map(getData,subList)
   resultList = joinResultLists(resArr)
-  print 'Saving results...'
   writeResultsList(resultList)
-  print 'done.' 
-  print "Completed." 
+  print "Completed."
+
+  downLine = 'Downloaded: ' + str(currEnd) + '/' + str(maxL) + '\n'
+  totalLine = 'Total time: ' + str(tdeltaMS) + '\n'
+  queryLine = 'Query time: ' + str(tdeltaMS0) + '\n'
+  threadsLine = 'Threads number: ' + str(step) + '\n'
+  f_name = 'dicomClientTime.txt'
+  f = open(f_name,'a')
+  f.write(downLine)
+  f.write(totalLine)
+  f.write(queryLine)
+  f.write(threadsLine)
+  f.close()
+
+  print downLine 
+  print totalLine
+  print queryLine
+  print threadsLine 
     
 if __name__=="__main__":main()
